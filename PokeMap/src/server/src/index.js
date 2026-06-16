dc83c9e75439e2ff19a89fa0ae49958e8533784c
nguyendwctrung <nguyentrung26gl@gmail.com>
2026-06-16 17:27:18 +0700

    sth

--- PokeMap/src/server/src/index.js
+++ PokeMap/src/server/src/index.js
@@ -14,8 +14,6 @@ import AdminRouter from "./routes/admin/index.route.js";
 const app = express();
 const PORT = 10000;
 
-const stripe = require('stripe')('sk_live_4eC39HqLyjWDarjtT1zdp7dc');
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
